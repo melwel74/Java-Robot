b0230161f209f33d239ae51b58d25e8cd5412d40
Melvin Welton <nivlem65@gmail.com>
2024-09-16 15:12:26 -0500

    State

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,7 @@
+
+var playerMoney = 10;
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -8,6 +12,11 @@ console.log(playerName, playerAttack, playerHealth);
 var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+// "LOSE" - Player robot's health is zero or less
 
 var fight = function () {
   // Alert players that they are starting the round
@@ -44,9 +53,19 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
   // if player choses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
-  window.alert(playerName + " has chosen to skip the fight!");
-} else {
-  window.alert("You need to choose a valid option. Try again!");
+  // confirm player wants to skip
+  var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+  }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
 }
 
 };
